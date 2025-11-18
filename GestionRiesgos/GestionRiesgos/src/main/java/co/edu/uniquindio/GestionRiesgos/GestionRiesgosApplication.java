0985afacecf0928a32d095822025f93a5cc19515
raquellopez7928 <raquel.lopeza@uqvirtual.edu.co>
2025-11-18 12:50:09 -0500

    Feat: Creación Enums y carpeta Spring-boot

---
+++ GestionRiesgos/GestionRiesgos/src/main/java/co/edu/uniquindio/GestionRiesgos/GestionRiesgosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.GestionRiesgos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRiesgosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionRiesgosApplication.class, args);
+	}
+
+}
---
+++ GestionRiesgos/GestionRiesgos/src/test/java/co/edu/uniquindio/GestionRiesgos/GestionRiesgosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.GestionRiesgos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRiesgosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
